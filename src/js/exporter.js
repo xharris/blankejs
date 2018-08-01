00ed40574e2693b4b6abc673354a03d59c06dce4
Xavier Harris <xharris1@abedin.irc.umbc.edu>
2018-08-01 14:09:40 -0400

    working on exporter

--- src/js/exporter.js
+++ src/js/exporter.js
@@ -13,7 +13,23 @@ class Exporter extends Editor {
 
 		var this_ref = this;
 
+		this.platforms = ['windows','mac','linux'];
+
+		this.el_platforms = app.createElement("div","platforms");
+		let el_title1 = app.createElement("p","title1");
+		el_title1.innerHTML = "One-click export";
+		this.el_platforms.appendChild(el_title1);
 		
+		for (let p of this.platforms) {
+			let el_platform_container = app.createElement("button",["ui-button-rect","platform",p]);
+			let el_platform_icon = app.createElement("img","icon");
+			el_platform_icon.src = "icons/windows.png";
+
+			el_platform_container.appendChild(el_platform_icon);
+			this.el_platforms.appendChild(el_platform_container);
+		}
+
+		this.appendChild(this.el_platforms);
 	}
 }
 
