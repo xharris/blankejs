d0e79d4dbd981df985eacae2f322c62e151de09a
xharris <xhh950@gmail.com>
2019-02-11 18:13:34 -0500

    last commit didn't include correct files...

---
+++ src/js/plugin.js
@@ -0,0 +1,42 @@
+let lua_plugins = [];
+let js_plugins = [];
+
+// get a list of .lua and .js files
+function refreshPluginList() {
+	nwFS.readdir(app.settings.plugin_path, (err, files) => {
+		for (let f of files) {
+			if (f.endsWith('.lua')) {
+				lua_plugins.push(f);
+			}
+			if (f.endsWith('.js')) {
+				js_plugins.push(f);
+			}
+		}
+		installPlugins();
+	});
+}
+
+function installPlugins() {
+	// move lua files to <engine_path>/lua/blanke/plugins/
+	let eng_plugin_dir = nwPATH.join(app.settings.engine_path,'lua','blanke','plugins');
+	nwFS.emptyDir(eng_plugin_dir, err => {
+		if (err) return console.error(err);
+		for (let f of lua_plugins) {
+			nwFS.copyFileSync(nwPATH.join(app.settings.plugin_path,f), nwPATH.join(eng_plugin_dir,f));
+		}
+	});
+
+	// add .js files to ide somehow
+	// ...
+}
+
+document.addEventListener("openProject",function(e){
+	refreshPluginList();
+});
+
+
+document.addEventListener("ideReady",function(e){
+	let plugin_watch = nwFS.watch(app.settings.plugin_path, function(evt_type, file) {
+		refreshPluginList();
+	});
+});
