a4f4348e301994d2229128c757236579d8b64fee
xharris <xhh950@gmail.com>
2019-03-22 14:48:42 -0400

    cleaned up BlankE.lua

--- projects/battleroyale/conf.lua
+++ projects/battleroyale/conf.lua
@@ -7,7 +7,7 @@ function love.conf(t)
     t.externalstorage = false           -- True to save files (and read from the save directory) in external storage on Android (boolean) 
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
  
-    t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
+    --t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
  
     t.window.title = "Untitled"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
