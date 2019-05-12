abe8b4163e611199c56aa27b7f965313172f281f
xharris <xhh950@gmail.com>
2019-05-12 15:05:23 -0400

    added Sprite and Input

--- blankejs/blanke.js
+++ blankejs/blanke.js
@@ -1,4 +1,5 @@
 var Blanke = (selector, options) => {
+    let blanke_ref = this;
     this.options = options || {}; // TODO change to table.update or whataever
     // init PIXI
     let app = new PIXI.Application({
@@ -10,6 +11,16 @@ var Blanke = (selector, options) => {
     // add main container for game
     let game_container = new PIXI.Container();
     app.stage.addChild(game_container);
+    // focus/blur events
+    this.focused = false;
+    app.view.tabIndex=-1;
+    app.view.style.outline="none";
+    app.view.addEventListener('focus',(e)=> {
+        blanke_ref.focused = true;
+    });
+    app.view.addEventListener('blur',(e)=> {
+        blanke_ref.focused = false;
+    });
     
     /* -ASSET */
     class Asset {
@@ -37,7 +48,7 @@ var Blanke = (selector, options) => {
                 return;
             switch (type) {
                 case 'image':
-                    return Asset.data[type][name].clone();
+                    return Asset.data[type][name];//.clone();
                     break;
             }
         }
@@ -53,15 +64,19 @@ var Blanke = (selector, options) => {
         constructor (name) {
             this.container = new PIXI.Container();
             this.onStart = () => {};
+            this.onUpdate = (dt) => {};
             this.onEnd = () => {};
             game_container.addChild(this.container);
         }
     
         _onStart () {
             this.onStart();
+            // start update loop
+            app.ticker.add(this.onUpdate, this);
         }
     
         _onEnd () {
+            app.ticker.remove(this.onUpdate, this);
             this.onEnd();
             this.container.destroy();
         }
@@ -76,7 +91,7 @@ var Blanke = (selector, options) => {
             Scene.ref[name] = new _Scene(name);
             // apply given callbacks
             for (let fn in functions) {
-                Scene.ref[name][fn] = functions[fn];
+                Scene.ref[name][fn] = functions[fn].bind(Scene.ref[name]);
             }
         }
         return Scene.ref[name];
@@ -105,7 +120,7 @@ var Blanke = (selector, options) => {
     }
     
     Scene.addDrawable = (obj) => {
-        if (Scene.stack.length > 0)
+        if (Scene.stack.length > 0) 
             Scene(Scene.stack[Scene.stack.length-1]).container.addChild(obj);
     }
     
@@ -115,8 +130,6 @@ var Blanke = (selector, options) => {
     /* -SPRITE */
     class Sprite {
         constructor (name, options) {
-            this.x = 0;
-            this.y = 0;
             this.animated = false;
             
             if (name)
@@ -134,6 +147,11 @@ var Blanke = (selector, options) => {
             this.sprite.x = 0;
             this.sprite.y = 0;
         }
+
+        get x () { return this.sprite.x };
+        get y () { return this.sprite.y };
+        set x (v) { if (this.sprite) this.sprite.x = v; }
+        set y (v) { if (this.sprite) this.sprite.x = v; }
     
         crop (x, y, w, h) {
             /*
@@ -146,5 +164,70 @@ var Blanke = (selector, options) => {
         }
     }
 
-    return {Asset, Scene, Sprite};
+    /* -INPUT */
+    let key_translation = {
+        'down':'ArrowDown',
+        'left':'ArrowLeft',
+        'right':'ArrowRight',
+        'up':'ArrowUp'
+    };
+    let keys_pressed = {};  // { 'ArrowLeft': true, 'a': false }
+    let keys_released = {}; // ...
+    let input_ref = {};     // { bob: ['left','a'] }
+    let release_check = {}; // { 'ArrowLeft': false, 'a': true }
+    window.addEventListener('keyup',(e)=>{
+        let key = e.key;
+        keys_pressed[key] = false;
+        keys_released[key] = true;
+        release_check[key] = false;
+    });
+    window.addEventListener('keydown',(e)=>{
+        let key = e.key;
+        keys_pressed[key] = true;
+        keys_released[key] = false;
+    });
+    var Input = (name) => {
+        let ret = { pressed: false, released: false };
+        let inputs = input_ref[name];
+        if (!inputs) return;
+        for (let i_str of inputs) {
+            // multi-inputs?
+            let i_list = i_str.split('+');
+            let all_pressed = true;
+            let all_released = true;
+            for (let i_part of i_list) {
+                if (!keys_pressed[i_part])
+                    all_pressed = false;
+                if (!keys_released[i_part])
+                    all_released = false;
+            }
+            if (all_pressed)
+                ret.pressed = true;
+            if (all_released)
+                ret.released = true;
+        }
+        return ret;
+    };
+    Input.set = (name, ...input) => {
+        let inputs = [];
+        for (let i of input) {
+            if (key_translation[i])
+                inputs.push(key_translation[i]);
+            else
+                inputs.push(i);
+        }
+        input_ref[name] = inputs;
+    };
+    Input.releaseCheck = () => {
+        for (let key in keys_released) {
+            if (release_check[key] == false && keys_released[key] == true) {
+                release_check[key] = true;
+                keys_released[key] = false;
+            }
+        }
+    }
+    app.ticker.add(()=>{
+        Input.releaseCheck();
+    },null,PIXI.UPDATE_PRIORITY.LOW);
+    return {Asset, Input, Scene, Sprite};
 }
