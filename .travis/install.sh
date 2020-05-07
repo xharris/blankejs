7f856fc84bf10e3f34562dccb4fc378ec49c550f
xharris <xhh950@gmail.com>
2020-05-06 23:35:44 -0400

    trying new travis config

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=':99.0'
+  export DISPLAY=:99.0
   sh -e /etc/init.d/xvfb start
-  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
   sleep 3
 fi
